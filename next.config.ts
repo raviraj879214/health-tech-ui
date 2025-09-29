51777ceea2dcec51f7ca83e399459f9a72b65055
raviraj879214 <ravirajalphainfo@gmail.com>
2025-09-29 16:06:06 +0530

    fgg

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ["@svgr/webpack"],
     });
     return config;
+  },
+   eslint: {
+    ignoreDuringBuilds: true, // Disable ESLint during production build
   },
 };
 
